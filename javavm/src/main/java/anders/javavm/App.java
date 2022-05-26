d89da4ad517c9fedcd1c4a0265744c173291f63c
sunchey11 <sunchey11@users.noreply.github.com>
2022-05-26 11:20:59 +0800

    创建工程

---
+++ javavm/src/main/java/anders/javavm/App.java
@@ -0,0 +1,13 @@
+package anders.javavm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
